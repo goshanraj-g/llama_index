826965334c189d68581b30c5a4193bcb650be7ca
Goshanraj Govindaraj <govindag@mcmaster.ca>
2025-07-14 16:02:03 -0400

    feat: add extract support to TavilyToolSpec

--- llama-index-integrations/tools/llama-index-tools-tavily-research/llama_index/tools/tavily_research/base.py
+++ llama-index-integrations/tools/llama-index-tools-tavily-research/llama_index/tools/tavily_research/base.py
@@ -9,9 +9,7 @@ from llama_index.core.tools.tool_spec.base import BaseToolSpec
 class TavilyToolSpec(BaseToolSpec):
     """Tavily tool spec."""
 
-    spec_functions = [
-        "search",
-    ]
+    spec_functions = ["search", "extract"]
 
     def __init__(self, api_key: str) -> None:
         """Initialize with parameters."""
@@ -39,4 +37,52 @@ class TavilyToolSpec(BaseToolSpec):
         return [
             Document(text=result["content"], extra_info={"url": result["url"]})
             for result in response["results"]
+        ]
+
+    def extract(
+        self,
+        url: str,
+        include_images: bool = False,
+        include_favicon: bool = False,
+        extract_depth: str = "advanced",
+        format: str = "markdown",
+    ) -> List[Document]:
+        """
+        Extract raw content from a URL using Tavily Extract API
+
+        Args:
+            url: the URL to extract content from
+            include_images: Whether to include images in the response
+            include_favicon: Whether to include the favicon in the response
+            extract_depth: 'basic' or 'advanced' (default -> advanced)
+            format: 'markdown' or 'text' (default -> markdown)
+
+        Returns:
+            A list with one Document containing the extracted content and metadata, or an empty list if no results were returned
+        """
+
+        response = self.client.extract(
+            url,
+            extract_depth=extract_depth,
+            include_images=include_images,
+            include_favicon=include_favicon,
+            format=format,
+        )
+
+        results = response.get("results", [])
+
+        if not results:
+            return []
+
+        result = results[0]
+
+        return [
+            Document(
+                text=result["raw_content", ""],
+                extra_info={
+                    "url": result.get("url"),
+                    "favicon": result.get("favicon"),
+                    "images": result.get("images"),
+                },
+            )
         ]
